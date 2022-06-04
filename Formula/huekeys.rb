2fc1d5657b80cd5196467d39ef095ecfe3c976dd
BitPony Tap <bradrf+brewtap@bitpony.com>
2022-06-04 23:34:34 +0000

    diff --git a/Formula/huekeys.rb b/Formula/huekeys.rb index dff5ef9..986d002 100644 --- a/Formula/huekeys.rb +++ b/Formula/huekeys.rb @@ -3,9 +3,9 @@  class Huekeys < Formula    desc 'Control the keyboard backlight on System76 laptops'    homepage 'https://github.com/BitPonyLLC/huekeys' -  url 'https://github.com/BitPonyLLC/huekeys/releases/download/0.4.1/huekeys' -  version "0.4.1" -  sha256 "696e94486b918791f21117b6659bcec7cdea2a6774a95342afeeaaba76bb65e7" +  url 'https://github.com/BitPonyLLC/huekeys/releases/download/v0.4.4/huekeys' +  version "v0.4.4" +  sha256 "316a9f9ae70934d8d592a559f0d5c77969ed8c411cb505826d6627b7a674b204"    license 'MIT'
       depends_on :linux
    @@ -15,6 +15,6 @@ class Huekeys < Formula
       end
    
       test do
    -    assert_match "0.4.1", shell_output("#{bin}/huekeys --version", 2)
    +    assert_match "v0.4.4", shell_output("#{bin}/huekeys --version", 2)
       end
     end

--- Formula/huekeys.rb
+++ Formula/huekeys.rb
@@ -3,9 +3,9 @@
 class Huekeys < Formula
   desc 'Control the keyboard backlight on System76 laptops'
   homepage 'https://github.com/BitPonyLLC/huekeys'
-  url 'https://github.com/BitPonyLLC/huekeys/releases/download/0.4.1/huekeys'
-  version "0.4.1"
-  sha256 "696e94486b918791f21117b6659bcec7cdea2a6774a95342afeeaaba76bb65e7"
+  url 'https://github.com/BitPonyLLC/huekeys/releases/download/v0.4.4/huekeys'
+  version "v0.4.4"
+  sha256 "316a9f9ae70934d8d592a559f0d5c77969ed8c411cb505826d6627b7a674b204"
   license 'MIT'
 
   depends_on :linux
@@ -15,6 +15,6 @@ class Huekeys < Formula
   end
 
   test do
-    assert_match "0.4.1", shell_output("#{bin}/huekeys --version", 2)
+    assert_match "v0.4.4", shell_output("#{bin}/huekeys --version", 2)
   end
 end
