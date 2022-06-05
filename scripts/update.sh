539d32de3ea004ab73ab2185a74afa3952c52bd9
bradrf <brad@bitpony.com>
2022-06-05 10:44:14 -0700

    use same desc from huekeys

--- scripts/update.sh
+++ scripts/update.sh
@@ -7,9 +7,11 @@ name=$(gh release view -R BitPonyLLC/huekeys --json name -q .name)
 gh release download -R BitPonyLLC/huekeys "$name" -p huekeys
 
 sum=$(sha256sum huekeys | awk '{print $1}')
+desc=$(chmod 555 huekeys && ./huekeys dump desc)
 rm -f huekeys
 
 sed -i \
+    -e 's/^\(\s*desc\) .*$/\1 "'"${desc}"'"/' \
     -e 's|^\(\s*url .*download\)/[^/]*|\1/'${name}'|' \
     -e 's/^\(\s*version\) .*$/\1 "'${name}'"/' \
     -e 's/^\(\s*sha256\) .*$/\1 "'${sum}'"/' \
