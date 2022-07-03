7d93d36b24fbe5084ab1eca7f655d0fbfb74b9e0
BitPony Tap <bradrf+brewtap@bitpony.com>
2022-07-03 21:16:04 +0000

    v0.5.7

--- Formula/huekeys.rb
+++ Formula/huekeys.rb
@@ -3,9 +3,9 @@
 class Huekeys < Formula
   desc 'Control the keyboard backlight on System76 laptops'
   homepage 'https://github.com/BitPonyLLC/huekeys'
-  url 'https://github.com/BitPonyLLC/huekeys/releases/download/v0.5.5/huekeys'
-  version "v0.5.5"
-  sha256 "bcaa25dc9068237c6882818c589035b075f821034b64c494e892ecbffe32363f"
+  url 'https://github.com/BitPonyLLC/huekeys/releases/download/v0.5.7/huekeys'
+  version "v0.5.7"
+  sha256 "a9eec1b3af59b24f9025c3ee0e9535ea3108fcd4ea53c443b3e16443d7c87b14"
   license 'MIT'
 
   depends_on :linux
